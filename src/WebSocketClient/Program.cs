e1434845ddeed1bc6764cafa20612402ff8ef472
Kerry Jiang <kerry-jiang@hotmail.com>
2020-03-14 01:02:48 -0700

    client

---
+++ src/WebSocketClient/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        private static WebSocketInfo[] _websocketInfoArr;
+
+        static async Task Main(string[] args)
+        {
+            var connCount = int.Parse(args[0]);
+
+            _websocketInfoArr = Enumerable
+                .Range(0, connCount)
+                .Select(i => new WebSocketInfo())
+                .ToArray();
+
+            var uri = new Uri(args[1]);
+
+            await AsyncParallel.ForEach(_websocketInfoArr, async w =>
+            {
+                await RunClient(w, uri);
+            }, connCount);
+
+            foreach (var w in _websocketInfoArr)
+            {
+                Console.WriteLine($"{w.SessionID}: {w.Total}");
+            }
+        }
+
+
+        private static async ValueTask RunClient(WebSocketInfo websocketInfo, Uri uri)
+        {
+            var websocket = new ClientWebSocket();
+
+            try
+            {
+                await websocket.ConnectAsync(uri, CancellationToken.None);
+                Console.WriteLine("Connected");
+            }
+            catch
+            {
+                return;
+            }            
+
+            var buffer = new byte[1024 * 5];
+
+            var result = await websocket.ReceiveAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), CancellationToken.None);
+
+            websocketInfo.SessionID = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+            while (true)
+            {
+                try
+                {
+                    result = await websocket.ReceiveAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), CancellationToken.None);
+                }
+                catch (WebSocketException)
+                {
+                    break;
+                }                
+
+                if (result.CloseStatus != null || result.Count == 0)
+                    break;
+
+                websocketInfo.Total += result.Count;
+            }            
+        }
+    }
+}
---
+++ src/WebSocketClient/WebSocketInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Net.WebSockets;
+
+namespace WebSocketClient
+{
+    public class WebSocketInfo
+    {
+        public string SessionID { get; set; }
+
+        public int Total { get; set; }
+    }
+}
